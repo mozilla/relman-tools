8fdcab9493ea55e4cbe581ac166ad7a1ff46eb83
Aki Sasaki <aki@escapewindow.com>
2013-10-25 17:19:59 -0700

    copy in my edited files in, since there was weirdness in the diffs

--- merge_day_scripts/beta2release.py
+++ merge_day_scripts/beta2release.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
- 
+
 """A merge day helper script for Beta-->Release mogration to go along with https://wiki.mozilla.org/Release_Management/Merge_Documentation"""
- 
+
 import urllib2
 import os
 import re
@@ -16,7 +16,7 @@ def getTemplateValue(url):
     template_page = urllib2.urlopen(url).read().replace('\n', '')
     parsed_template = version_regex.match(template_page)
     return parsed_template.groups()[0]
- 
+
 def file_replace(fname, pat, s_after):
     try:
         with open(fname) as f:
@@ -31,14 +31,14 @@ def file_replace(fname, pat, s_after):
             out.close()
             os.rename(out_fname, fname)
         return True
- 
+
     except IOError:
         print fname, "does not exist"
         return False
-    
+
 
 def get_user_input():
-    
+
     #Reading Version number
     version = raw_input("Enter the current Release Version ( eg : 24.0 , including a chemspill version if exists) \n")
     print "you entered ", version
@@ -67,11 +67,14 @@ def tag_repo(repo, tag, rev, user):
 def commit_repo(repo, user, old_head, new_head):
     try:
         print " Commiting mozilla repo " + repo
-        call('hg -R '+ repo +' commit --close-branch -m " Closing old head. CLOSED TREE a=release"' + ' -u ' + user, shell=True)
+        # "Merge" the old head, rather than closing it, to avoid
+        # non-fastforward issues in vcs-sync
+        call('hg -R %s hgdebugsetparents %s %s' % (repo, new_head, old_head))
+        call('hg -R %s commit -m "Merging old head via |hg debugsetparents %s %s|. CLOSED TREE a=release" -u %s' % (repo, new_head, old_head, user), shell=True)
     except:
         print "commit_repo: Unexpected error"
         raise
-    
+
 def pull_up_repo(repo_beta, repo_release):
     try:
         print "pulling all changes from mozilla-beta into into mozilla-release\n"
@@ -88,23 +91,23 @@ def main():
     call('hg clone http://hg.mozilla.org/releases/mozilla-beta', shell=True)
     mozilla_release = "./mozilla-release/"
     mozilla_beta = "./mozilla-beta/"
-    
+
     version, hg_user = get_user_input()
     user_input = raw_input("Enter yes to begin with beta -> release merge day changes or no to exit\n")
     if user_input.lower() != "yes":
         print "Exiting now\n"
         return
-    
+
     #Getting version numbers
     beta_rev = get_rev(mozilla_beta)
     release_rev = get_rev(mozilla_release)
-    
+
     #Calculating tags
     now = datetime.datetime.now()
     date = now.strftime("%Y%m%d")
     release_base_tag = "RELEASE_BASE_" + date
     release_tag = "FIREFOX_RELEASE_" + version
-    
+
     #Tagging
     tag_repo(mozilla_beta, release_base_tag, beta_rev, hg_user)
     print "You have finished tagging mozilla-beta, now go ahead and push the mozilla-beta repo\n"
@@ -114,27 +117,25 @@ def main():
         print "Exiting now\n"
         return
     tag_repo(mozilla_release, release_tag, release_rev, hg_user)
-    
+
     #Commit,pull and update
-    #commit_repo(mozilla_release, hg_user)
     mozilla_release_revision = get_rev(mozilla_release)
     mozilla_beta_revision = get_rev(mozilla_beta)
     pull_up_repo(mozilla_beta, mozilla_release)
-    
     commit_repo(mozilla_release, hg_user, mozilla_release_revision, mozilla_beta_revision)
-    
-    
+
+
 #Edit desktop config
 
     raw_input("If you are ready to start with mozconfig changes, hit 'return' to continue...")
 
     if not file_replace(mozilla_release+"browser/confvars.sh", "ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-beta,firefox-mozilla-release", "ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-release"):
         print "Failed to replace channel ids in desktop config"
-    
+
     if not file_replace(mozilla_release+"browser/confvars.sh", "MAR_CHANNEL_ID=firefox-mozilla-beta", "MAR_CHANNEL_ID=firefox-mozilla-release"):
         print "Failed to replace MAR_CHANNEL_ID in desktop config"
 
-#Edit mobile mozconfigs 
+#Edit mobile mozconfigs
 
     release_branding_dirs = ["mobile/android/config/mozconfigs/android/","mobile/android/config/mozconfigs/android-armv6/","mobile/android/config/mozconfigs/android-x86/"]
     release_branding_files = ["release","l10n-release","l10n-nightly","nightly"]
@@ -150,8 +151,8 @@ def main():
     call('hg diff -R '+ mozilla_release , shell=True)
     raw_input("if the diff looks good hit return to continue to commit")
     call('hg commit -R '+ mozilla_release + ' -u ' + hg_user +' -m "Updating configs CLOSED TREE a=release ba=release"', shell=True)
-        
-    raw_input("Go ahead and push mozilla-release changes and you are done.")            
+
+    raw_input("Go ahead and push mozilla-release changes and you are done.")
 
 if __name__ == "__main__":
     main()
